af2bb6ea25dd06ca81a33aa0827ea0a73555d758
Victor Souza <victorsouza0106@gmail.com>
2025-05-10 22:52:57 -0300

    feat: add defaul route

--- routes.js
+++ routes.js
@@ -2,4 +2,6 @@ import { lobbyRouter } from "./lobby/views/lobby.js";
 
 export function getRoutes(app) {
   app.use("/lobby", lobbyRouter);
+
+  app.use("", (req, res) => res.status(200).send("WORKING"));
 }
